04e74a0237b2fd054c552ec18f76c8bf7ac13fbd
Elias Guere <eliasguere1025@gmail.com>
2020-03-13 22:02:32 -0500

    Layouts de las paginas creados

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ import Home from './pages/Home';
 import Contact from './pages/Contact';
 
 function App() {
-
   return (
-    <div className="app">
+    <div className='app'>
       <h1>Web personal - Client</h1>
       <Admin />
       <Signin />
