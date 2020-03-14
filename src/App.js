fd216724650afdba1aa8c283df22cd849c1f5158
Elias Guere <eliasguere1025@gmail.com>
2020-03-13 21:43:18 -0500

    Generando las paginas en react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { DatePicker, Card } from 'antd'
+
 import './App.scss';
+import Admin from './pages/Admin';
+import Signin from './pages/Admin/Signin';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
 
 function App() {
-
-  const test = (date, dateString) => {
-    console.log(date, dateString)
-  }
 
   return (
     <div className="app">
       <h1>Web personal - Client</h1>
-      <DatePicker onChange={test}></DatePicker>
-      <Card title="Default size card" style={{ width: 300 }}>
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
-      </Card>
+      <Admin />
+      <Signin />
+      <Home />
+      <Contact />
     </div>
   );
 }
