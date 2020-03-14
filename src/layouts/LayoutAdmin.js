1f0bb72d2d5022db089ca37a391c5948ebe1f294
Elias Guere <eliasguere1025@gmail.com>
2020-03-14 14:12:04 -0500

    Carga del varios componentes dentro del LayoutAdmin con el sistema de rutas

--- src/layouts/LayoutAdmin.js
+++ src/layouts/LayoutAdmin.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import { Layout } from 'antd';
 
-export default function LayoutAdmin() {
+import './LayoutAdmin.scss';
+
+export default function LayoutAdmin(props) {
+  const { routes } = props;
+  const { Header, Content, Footer } = Layout;
+
+  console.log('Estas son las props: ', props);
+
   return (
     <Layout>
-      <h2>Menu Sider</h2>
-      <div>Contenido....</div>
-      <h5>Footer</h5>
+      <h2>Menu Sider Admin</h2>
+      <Layout>
+        <Header>Header ...</Header>
+        <Content>
+          <LoadRouters routes={routes} />
+        </Content>
+        <Footer>Elias Guere Canchucaja</Footer>
+      </Layout>
     </Layout>
   );
+}
+
+function LoadRouters({ routes }) {
+  return routes.map((route, index) => {
+    return (
+      <Route
+        key={index}
+        path={route.path}
+        exact={route.exact}
+        component={route.component}
+      />
+    );
+  });
 }
