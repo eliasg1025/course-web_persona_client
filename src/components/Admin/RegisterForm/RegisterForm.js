0a82a0089ed1ca0ef2276a47db1caa22511e0b7e
Elias Guere <eliasguere1025@gmail.com>
2020-03-22 15:01:00 -0500

    Agregando funciones para la validacion del formulario de registro

--- src/components/Admin/RegisterForm/RegisterForm.js
+++ src/components/Admin/RegisterForm/RegisterForm.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input, Button, Checkbox, notification } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 import './RegisterForm.scss';
 
 export default function RegisterForm() {
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+    repeatPassword: '',
+    privacyPolicy: false
+  });
+
+  const changeForm = e => {
+    if (e.target.name === 'privacyPolicy') {
+      setInputs({
+        ...inputs,
+        [e.target.name]: e.target.checked
+      });
+    } else {
+      setInputs({
+        ...inputs,
+        [e.target.name]: e.target.value
+      });
+    }
+  };
+
+  const register = e => {
+    e.preventDefault();
+    console.log(inputs);
+  };
+
   return (
-    <Form className='register-form'>
+    <Form
+      className='register-form'
+      onSubmitCapture={register}
+      onChange={changeForm}
+    >
       <Form.Item>
         <Input
           prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
@@ -14,6 +44,7 @@ export default function RegisterForm() {
           name='email'
           placeholder='Correo electronico'
           className='register-form__input'
+          value={inputs.email}
         />
       </Form.Item>
       <Form.Item>
@@ -23,6 +54,7 @@ export default function RegisterForm() {
           name='password'
           placeholder='Contraseña'
           className='register-form__input'
+          value={inputs.password}
         />
       </Form.Item>
       <Form.Item>
@@ -32,7 +64,18 @@ export default function RegisterForm() {
           name='repeatPassword'
           placeholder='Repetir Contraseña'
           className='register-form__input'
+          value={inputs.repeatPassword}
         />
+      </Form.Item>
+      <Form.Item>
+        <Checkbox name='privacyPolicy' checked={inputs.privacyPolicy}>
+          He leido y acepto la politica de privacidad
+        </Checkbox>
+      </Form.Item>
+      <Form.Item>
+        <Button htmlType='submit' className='register-form__button'>
+          Crear cuenta
+        </Button>
       </Form.Item>
     </Form>
   );
