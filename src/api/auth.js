9d508d0e4c2cc76f7df303117c9a3440eabca3d7
Elias Guere <eliasguere1025@gmail.com>
2020-03-31 11:39:11 -0500

    Creando funcion para refrescar AccessToken desde el cliente

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,30 +2,62 @@ import { basePath, apiVersion } from './config';
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '../utils/constants';
 import jwtDecode from 'jwt-decode';
 
-export function getAccessToken() {
-  const accessToken = localStorage.getItem(ACCESS_TOKEN);
+export function getAccessTokenApi() {
+    const accessToken = localStorage.getItem(ACCESS_TOKEN);
 
-  if (!accessToken || accessToken == null) {
-    return null;
-  }
+    if (!accessToken || accessToken == null) {
+        return null;
+    }
 
-  return willExpiredToken(accessToken) ? null : accessToken;
+    return willExpiredToken(accessToken) ? null : accessToken;
 }
 
-export function getRefreshToken() {
-  const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+export function getRefreshTokenApi() {
+    const refreshToken = localStorage.getItem(REFRESH_TOKEN);
 
-  if (!refreshToken || refreshToken == null) {
-    return null;
-  }
+    if (!refreshToken || refreshToken == null) {
+        return null;
+    }
 
-  return willExpiredToken(refreshToken) ? null : refreshToken;
+    return willExpiredToken(refreshToken) ? null : refreshToken;
+}
+
+export function refreshAccessToken(refreshToken) {
+    const url = `${basePath}/${apiVersion}/refresh-access-token`;
+    const bodyObj = {
+        refreshToken: refreshToken
+    };
+
+    const params = {
+        method: 'POST',
+        body: JSON.stringify(bodyObj),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    fetch(url, params)
+        .then(response => {
+            if (response.status !== 200) {
+                return null;
+            }
+            return response.json();
+        })
+        .then(result => {
+            if (!result) {
+                // TO DO: deslogear usuario
+            } else {
+                const { accessToken, refreshToken } = result;
+                localStorage.setItem(ACCESS_TOKEN, accessToken);
+                localStorage.setItem(REFRESH_TOKEN, refreshToken);
+            }
+        });
 }
 
 function willExpiredToken(token) {
-  const seconds = 60;
-  const metaToken = jwtDecode(token);
-  const { exp } = metaToken;
-  const now = (Date.now() + seconds) / 1000;
-  return now > exp;
+    const seconds = 60;
+    const metaToken = jwtDecode(token);
+    const { exp } = metaToken;
+    const now = (Date.now() + seconds) / 1000;
+    return now > exp;
 }
