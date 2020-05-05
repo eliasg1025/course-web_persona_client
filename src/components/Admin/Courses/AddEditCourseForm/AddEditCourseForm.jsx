a8c694d950af29d79a3384e573072c7287045e42
Elias Guere <eliasguere1025@gmail.com>
2020-05-05 15:06:30 -0500

    Agregando funcionalidad de crear cursos

--- src/components/Admin/Courses/AddEditCourseForm/AddEditCourseForm.jsx
+++ src/components/Admin/Courses/AddEditCourseForm/AddEditCourseForm.jsx
@@ -1,35 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import { Form, Input, Button, notification } from 'antd';
-import { KeyOutlined, GifOutlined, DollarCircleOutlined, LinkOutlined } from '@ant-design/icons';
+import React, { useState, useEffect } from "react";
+import { Form, Input, Button, notification } from "antd";
+import {
+    KeyOutlined,
+    GifOutlined,
+    DollarCircleOutlined,
+    LinkOutlined,
+} from "@ant-design/icons";
 
-import { getAccessTokenApi } from '../../../../api/auth';
+import { getAccessTokenApi } from "../../../../api/auth";
+import { addCourseApi } from "../../../../api/course";
 
-import './AddEditCourseForm.scss';
+import "./AddEditCourseForm.scss";
 
 export default function AddEditCourseForm(props) {
     const { setIsVisibleModal, setReloadCourses, course } = props;
     const [courseData, setCourseData] = useState({});
 
+    const addCourse = (e) => {
+        e.preventDefault();
+
+        if (!courseData.idCourse) {
+            notification['warning']({
+                message: 'El id del curso es obligatorio'
+            })
+        } else {
+            const accessToken = getAccessTokenApi();
+
+            addCourseApi(accessToken, courseData)
+                .then(response => {
+                    const typeNotification = response.code === 200 ? 'success' : 'warning';
+
+                    notification[typeNotification]({
+                        message: response.message
+                    });
+
+                    setIsVisibleModal(false);
+                    setReloadCourses(true);
+                    setCourseData({});
+                })
+                .catch(err => {
+                    notification['error']({
+                        message: 'Error del servidor, intentelo mas tarde'
+                    });
+                })
+        }
+    };
+
+    const updateCourse = (e) => {
+        e.preventDefault();
+
+        console.log("Actualizando curso ...");
+    };
+
     return (
         <div className="add-edit-course-form">
             <AddEditForm
                 course={course}
+                addCourse={addCourse}
+                updateCourse={updateCourse}
+                courseData={courseData}
+                setCourseData={setCourseData}
             />
         </div>
     );
 }
 
 function AddEditForm(props) {
-    const { course } = props;
+    const {
+        course,
+        addCourse,
+        updateCourse,
+        courseData,
+        setCourseData,
+    } = props;
 
     return (
-        <Form className="form-add-edit" onSubmitCapture={() => console.log('Submit form ...')}>
+        <Form
+            className="form-add-edit"
+            onSubmitCapture={course ? updateCourse : addCourse}
+        >
             <Form.Item>
                 <Input
                     prefix={<KeyOutlined />}
                     placeholder="ID del curso"
-                    // value={}
-                    // onChange={}
+                    value={courseData.idCourse}
+                    onChange={(e) =>
+                        setCourseData({
+                            ...courseData,
+                            idCourse: e.target.value,
+                        })
+                    }
                     disabled={course ? true : false}
                 />
             </Form.Item>
@@ -37,29 +97,35 @@ function AddEditForm(props) {
                 <Input
                     prefix={<LinkOutlined />}
                     placeholder="Url del curso"
-                    // value={}
-                    // onChange={}
+                    value={courseData.url}
+                    onChange={(e) =>
+                        setCourseData({ ...courseData, link: e.target.value })
+                    }
                 />
             </Form.Item>
             <Form.Item>
                 <Input
                     prefix={<GifOutlined />}
                     placeholder="Cupón de descuento"
-                    // value={}
-                    // onChange={}
+                    value={courseData.coupon}
+                    onChange={(e) =>
+                        setCourseData({ ...courseData, coupon: e.target.value })
+                    }
                 />
             </Form.Item>
             <Form.Item>
                 <Input
                     prefix={<DollarCircleOutlined />}
                     placeholder="precio del curso"
-                    // value={}
-                    // onChange={}
+                    value={courseData.price}
+                    onChange={(e) =>
+                        setCourseData({ ...courseData, price: e.target.value })
+                    }
                 />
             </Form.Item>
             <Form.Item>
                 <Button type="primary" htmlType="submit" className="btn-submit">
-                    { course ? 'Actualizar curso' : 'Crear curso' }
+                    {course ? "Actualizar curso" : "Crear curso"}
                 </Button>
             </Form.Item>
         </Form>
