47ae7137efe34c7cd9e75c3b9f25dea263cc50d5
Elias Guere <eliasguere1025@gmail.com>
2020-05-10 16:44:55 -0500

    Obteniendo la informacion publica de cada curso de la API de Udemy

--- src/components/Web/Courses/CoursesList/CoursesList.jsx
+++ src/components/Web/Courses/CoursesList/CoursesList.jsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Row, Col, Card, Button, Rate, notification } from "antd";
+import { getCourseDataUdemyApi } from "../../../../api/course";
 
 import "./CoursesList.scss";
 
 export default function CoursesList(props) {
     const { courses } = props;
 
-    console.log(courses);
     return (
-        <div>
-            <h2>CoursesList ...</h2>
+        <div className="courses-list">
+            <Row>
+                {courses.map((course) => (
+                    <Col key={course._id} md={4} className="courses-list__course">
+                        <Course course={course} />
+                    </Col>
+                ))}
+            </Row>
         </div>
     );
+}
+
+function Course(props) {
+    const { course } = props;
+    const [courseInfo, setCourseInfo] = useState({});
+
+    useEffect(() => {
+        getCourseDataUdemyApi(course.idCourse)
+            .then(response => {
+                if (response?.code !== 200) {
+                    notification['warning']({
+                        message: response.message
+                    });
+                } else {
+                    setCourseInfo(response.data);
+                }
+            })
+            .catch(err => {
+                notification['error']({
+                    message: 'Error del servidor, intentelo más tarde'
+                });
+            });
+    }, [course]);
+
+    return <p>{ courseInfo.title }</p>;
 }
